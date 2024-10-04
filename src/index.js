d1ac557e054f03296301566adac5dbb5240f2156
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-10-04 20:36:13 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.get('/api/test', (req, res) => {
+  res.send({ message: 'Endpoint de teste'});
+});
+
+app.get('/api/test-2', (req, res) => {
+  res.send({ message: 'Endpoint de teste 2334'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`);
+});
+
+module.exports = app;
